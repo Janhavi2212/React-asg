9c30895ed59cc1748e171073cb40443c1c1fffbb
Janhavi2212 <janhavi.walde22@gmail.com>
2025-05-15 11:04:23 +0530

    weather-app

--- 15-05/weather-app/src/components/Weather.jsx
+++ 15-05/weather-app/src/components/Weather.jsx
@@ -2,9 +2,9 @@ import React, { useEffect, useState } from 'react'
 const Weather = () => {
 const [cityName,setCityName]= useState('')
 const [weatherData,setWeatherdata]=useState({})
-    const search = async (city) => {
+    const search = async (cityName) => {
         try {
-            const url =`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${import.meta.env.VITE_APP_ID}`;
+            const url =`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${import.meta.env.VITE_APP_ID}`;
             const res = await fetch(url)
             const data = await res.json()
             console.log(data)
@@ -14,7 +14,7 @@ const [weatherData,setWeatherdata]=useState({})
         }
     }
     const handleClick = ()=>{
-        setCity(cityName)
+        setCityName(cityName);
         search(cityName)
     }
   return (
@@ -27,8 +27,10 @@ const [weatherData,setWeatherdata]=useState({})
 
         <div className='weatherData'>
             <h2>{weatherData.name}</h2>
+            <p>Condition: {weatherData.weather[0]?.description}</p>
             <p>Temperature: {weatherData.main?.temp}°C</p>
             <p>Humidity: {weatherData.main?.humidity}%</p>
+            <p>Wind Speed: {weatherData.wind?.speed} m/s</p>
         </div>
     </div>
   )
